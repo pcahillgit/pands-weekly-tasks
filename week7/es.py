9360dd72f4768e6c50d00842c6c5940aec772bdf
Paul Cahill <paulcahill97@outlook.com>
2024-04-16 16:15:57 +0100

    Week 7 weekly task: Es

---
+++ week7/es.py
@@ -0,0 +1,32 @@
+#This program takes in a file as an argument on the command line and tells you how many times the letter e is in that file.
+#I'm assuming that all e's, capitalised or not, accented or not, need to be counted from start to finish of the doc.
+#Author: Paul Cahill
+
+#importing sys which will allow us to access sys.argv (command line arguments)
+import sys
+
+#importing unicodedata which will allow us to turn accented e's into normal ones.
+import unicodedata
+
+#opening the file in read mode, converting accented e's into normal ones, counting the e's and printing result.
+try:
+    access_file = sys.argv[1]
+#Accounting for non-text file:
+    if not access_file.endswith('.txt'):
+        print("File must be a .txt file, please try again.")
+    else:
+        with open(access_file, 'r') as file:
+            content = file.read()
+            #here, NFD is changing accented e's to normal ones
+            normalise_content = unicodedata.normalize('NFD', content)
+             # .lower converts the string to lower case
+            e_count = normalise_content.lower().count('e')
+            print(f"There are {e_count} e's in {access_file}.")
+
+#Accounting for no argument:
+except IndexError:
+    print ("No argument provided, please provide a file (example: es.py test.txt)")
+
+#Accounting for filename that doesnt exist:
+except FileNotFoundError:
+    print (f"File {access_file} not found, please try again.")
